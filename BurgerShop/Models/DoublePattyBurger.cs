bef0569e8abbf6053f71440471f9a34766418eff
MfundoZA <20117271@vcconnect.co.za>
2021-08-03 21:25:59 +0200

    Created subclasses of Burger class

---
+++ BurgerShop/Models/DoublePattyBurger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerShop.Models
+{
+    class DoublePattyBurger : Burger
+    {
+        private String secondPatty;
+
+        public DoublePattyBurger()
+            : base()
+        {
+        }
+
+        public DoublePattyBurger(string bun, string firstPatty, string secondPatty)
+            : base(bun, firstPatty)
+        {
+            this.secondPatty = secondPatty;
+        }
+
+        public string SecondPatty { get => secondPatty; set => secondPatty = value; }
+    }
+}
---
+++ BurgerShop/Models/VeggieBurger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BurgerShop.Models
+{
+    class VeggieBurger : Burger
+    {
+        private const string VEGGIE_PATTY = "Veggie Patty";
+        private String mushrooms;
+        private String lettuce;
+
+        public VeggieBurger()
+            : base()
+        {
+        }
+
+        public VeggieBurger(string bun, string mushrooms, string lettuce)
+            : base()
+        {
+            base.Bun = bun;
+            base.Patty = VEGGIE_PATTY;
+            this.mushrooms = mushrooms;
+            this.lettuce = lettuce;
+        }
+
+        public override string Patty { get => base.Patty; }
+        public string Mushrooms { get => mushrooms; set => mushrooms = value; }
+        public string Lettuce { get => lettuce; set => lettuce = value; }
+    }
+}
